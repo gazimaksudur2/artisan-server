6b4be68bd0ed59fba6dec8c1a36c4d0d84bb8ab0
GaziMaksudur <gazimaksudur2@gmail.com>
2024-04-29 00:31:07 +0600

    vercel issue solved'

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
+        // await client.connect();
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         const craftsDB = client.db("craftsDB").collection("crafts");
@@ -37,66 +37,73 @@ async function run() {
             res.send('Server is working perfectly!!!');
         })
 
-        app.post('/crafts',async(req, res)=>{
+        app.post('/crafts', async (req, res) => {
             const craft = req.body;
             // console.log(craft);
             const result = await craftsDB.insertOne(craft);
             res.send(result);
         });
 
-        app.get('/crafts',async(req, res)=>{
+        app.get('/crafts', async (req, res) => {
             const cursor = craftsDB.find();
             const crafts = await cursor.toArray();
             res.send(crafts);
         });
 
-        app.get('/crafts/:id',async(req, res)=>{
+        app.get('/crafts/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const craft = await craftsDB.findOne(query);
             res.send(craft);
         });
 
-        app.get('/crafts/subcategory/:subcategory',async(req, res)=>{
+        app.get('/crafts/subcategory/:subcategory', async (req, res) => {
             const subcategory = req.params.subcategory;
-            const query = {subcategory: subcategory};
+            const query = { subcategory: subcategory };
             const cursor = craftsDB.find(query);
             const craft = await cursor.toArray();
             res.send(craft);
         });
 
-        app.get('/crafts/user/:user',async(req, res)=>{
+        app.get('/crafts/user/:user', async (req, res) => {
             const user = req.params.user;
-            const query = {provider: user};
+            const query = { provider: user };
             const craft = await craftsDB.findOne(query);
             res.send(craft);
         });
 
-        app.get('/crafts/email/:email',async(req, res)=>{
+        app.get('/crafts/email/:email', async (req, res) => {
             const email = req.params.email;
-            const query = {provider_email: email};
+            const query = { provider_email: email };
             const cursor = craftsDB.find(query);
             const craft = await cursor.toArray();
             res.send(craft);
         });
 
-        app.put('/crafts/:id',async(req, res)=>{
+        app.put('/crafts/:id', async (req, res) => {
+            // console.log(req.body);
             const id = req.params.id;
             const { name, subcategory, rating, stock, customization, brief, price, url, provider, provider_email } = req.body;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const options = { upsert: true };
             const updatedDoc = {
-                $set:{
+                $set: {
                     name, subcategory, rating, stock, customization, brief, price, url, provider, provider_email
                 }
             }
-            console.log(req.body);
-            // const result = await craftsDB.updateOne(filter, updatedDoc, options);
+            const result = await craftsDB.updateOne(filter, updatedDoc, options);
+            res.send(result);
         })
 
-        app.listen(port,()=>{
-            console.log(`Server is running on port : ${port}`);
+        app.delete('/crafts/:id', async (req, res) => {
+            // console.log(req.body);
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await craftsDB.deleteOne(query);
+            res.send(result);
         })
+
+
 
     } finally {
         // Ensures that the client will close when you finish/error
@@ -106,3 +113,7 @@ async function run() {
 run().catch(console.dir);
 
 //MongoDB is not working showing error
+
+app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`);
+})
